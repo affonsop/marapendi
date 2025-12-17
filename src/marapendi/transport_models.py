8cd3f82c5c905838d46db7ca7f59b176676b715f
Pedro AFFONSO NOBREGA <pedro.affonso_nobrega@minesparis.psl.eu>
2025-12-17 15:06:05 +0100

    Correct wrong limit for capillary pressure.

--- src/marapendi/transport_models.py
+++ src/marapendi/transport_models.py
@@ -305,7 +305,7 @@ class DarcyTransportModel:
         float
             Estimated water saturation.
         """
-        return (capillary_pressure / layer.capillary_pressure_J_ratio) ** (1. / self.J_function_exponent)
+        return np.minimum((capillary_pressure / layer.capillary_pressure_J_ratio) ** (1. / self.J_function_exponent),1)
 
     def capillary_pressure_from_saturation(self, layer, non_wetting_saturation):
         """
@@ -323,7 +323,7 @@ class DarcyTransportModel:
         float
             Capillary pressure (Pa).
         """
-        return np.minimum((non_wetting_saturation ** self.J_function_exponent) * layer.capillary_pressure_J_ratio, 1)
+        return (non_wetting_saturation ** self.J_function_exponent) * layer.capillary_pressure_J_ratio
 
     def calculate_non_wetting_saturation(self, layer, non_wetting_flux, upstream_capillary_pressure=0): 
         """
@@ -353,18 +353,18 @@ class DarcyTransportModel:
         layer.upstream_saturation = self.saturation_from_capillary_pressure(layer, upstream_capillary_pressure)
 
         # Compute downstream saturation based on flux and flow resistance
-        layer.downstream_saturation = np.minimum(0.9, (
+        layer.downstream_saturation = np.maximum(0,np.minimum(0.9, (
             (layer.saturation_flow_resistance * non_wetting_flux) ** (1. / (layer.relative_permeability_exponent + self.J_function_exponent))
             + layer.upstream_saturation
-        ))
+        )))
 
         # Compute mean saturation in the layer
-        layer.non_wetting_saturation = (
+        layer.non_wetting_saturation = np.maximum(0,np.minimum(0.9,
             (layer.downstream_saturation - layer.upstream_saturation) *
             ( (layer.relative_permeability_exponent + self.J_function_exponent) /
               (layer.relative_permeability_exponent + self.J_function_exponent + 1))
             + layer.upstream_saturation
-        )
+        ))
 
         # Compute downstream capillary pressure
         layer.downstream_capillary_pressure = self.capillary_pressure_from_saturation(layer, layer.downstream_saturation)
